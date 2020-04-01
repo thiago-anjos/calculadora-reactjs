790224ce7a17c413f92d633d580c42927809f138
thiago-anjos <thiago.w3c@gmail.com>
2020-04-01 17:03:44 -0300

    Updates

---
+++ precache-manifest.5bebaec9a0cef826d85635a5c8dd5e9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d397cc5f871434197bdfc11f6caf5be9",
+    "url": "/calculadora-reactjs/index.html"
+  },
+  {
+    "revision": "f5eceaefdd4bb83bafa2",
+    "url": "/calculadora-reactjs/static/css/main.c9cfd601.chunk.css"
+  },
+  {
+    "revision": "eb49e68c631aa441ac25",
+    "url": "/calculadora-reactjs/static/js/2.20840165.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculadora-reactjs/static/js/2.20840165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5eceaefdd4bb83bafa2",
+    "url": "/calculadora-reactjs/static/js/main.594962c5.chunk.js"
+  },
+  {
+    "revision": "e2f8ec330c94bfb924dc",
+    "url": "/calculadora-reactjs/static/js/runtime-main.3575fadb.js"
+  }
+]);
